79cd6ed6097adb1256bdf6d2a59765996a8c6400
zaid-alrafayia <zaidomar678@gmail.com>
2026-02-04 02:38:52 +0300

    init

---
+++ philo/philo.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaalrafa <zaalrafa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 20:09:57 by zaalrafa          #+#    #+#             */
+/*   Updated: 2026/02/02 20:25:43 by zaalrafa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+#endif // !DEBUG
